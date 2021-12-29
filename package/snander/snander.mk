bd977fa713abc1653974ff025c958aebd4eaf257
Daniel Palmer <daniel@0x0f.com>
2021-12-29 22:47:06 +0900

    pkg-config fun time

--- package/snander/snander.mk
+++ package/snander/snander.mk
@@ -4,13 +4,13 @@
 #
 ################################################################################
 
-SNANDER_VERSION = 2ae5e5a9934c40f207ac5674ae691b00883f8b8a
+SNANDER_VERSION = 1d2442a94d71ddd0b73764317f775774aa2f9a8c
 SNANDER_SITE = https://github.com/fifteenhex/SNANDer.git
 SNANDER_SITE_METHOD = git
 HOST_SNANDER_DEPENDENCIES = host-pkgconf host-libusb host-libusb-compat
 
 define HOST_SNANDER_BUILD_CMDS
-	$(MAKE) -C $(@D)/src
+	$(MAKE) PKG_CONFIG=$(HOST_DIR)/bin/pkgconf -C $(@D)/src
 endef
 
 $(eval $(host-generic-package))
