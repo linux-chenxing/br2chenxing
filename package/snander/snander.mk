232f9b07584a8608f901f547e84b50f036d068fd
Daniel Palmer <daniel@0x0f.com>
2021-12-29 14:53:55 +0900

    SNANDer host deps

--- package/snander/snander.mk
+++ package/snander/snander.mk
@@ -7,7 +7,7 @@
 SNANDER_VERSION = 2ae5e5a9934c40f207ac5674ae691b00883f8b8a
 SNANDER_SITE = https://github.com/fifteenhex/SNANDer.git
 SNANDER_SITE_METHOD = git
-HOST_SNANDER_DEPENDENCIES = host-libusb
+HOST_SNANDER_DEPENDENCIES = host-pkgconf host-libusb host-libusb-compat
 
 define HOST_SNANDER_BUILD_CMDS
 	$(MAKE) -C $(@D)/src
